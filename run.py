0a99924189b4418100adbe6f693ad703275d2b0a
ifzal malook <ifzal.malook@gmail.com>
2024-02-20 21:43:13 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -60,9 +60,7 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
-    stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock[-1]
-    print(stock_row)
+    
 
 def calculate_surplus_data(sales_row):
     """
@@ -73,6 +71,15 @@ def calculate_surplus_data(sales_row):
      -Negative surplus indicates extra made when stock was sold out.
     """
     print ("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return(surplus_data)
 
 def main():
     """
@@ -83,6 +90,8 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 print ("Welcome to Love Sandwiches Data Automation")
 main()
